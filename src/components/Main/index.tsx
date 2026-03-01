7168d9acaa6d901538417cfe900e6eee015e85c6
Luan <luanitachi@hotmail.com>
2026-03-01 02:58:42 -0300

    create main hero and categories

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,75 @@
+import "./main.scss";
+import tecnologyIcon from "../../assets/icons/Tecnology.svg";
+import superMarketIcon from "../../assets/icons/supermercados 1.svg";
+import drinkIcon from "../../assets/icons/whiskey.svg";
+import toolsIcon from "../../assets/icons/ferramentas 1.svg";
+import healthIcon from "../../assets/icons/cuidados-de-saude 1.svg";
+import sportIcon from "../../assets/icons/corrida 1.svg";
+import fashionIcon from "../../assets/icons/moda 1.svg";
+export function Main() {
+  return (
+    <main className="main">
+      <section className="hero">
+        <div className="hero-content">
+          <h1>Venha conhecer nossas promoções</h1>
+          <p>
+            50% Off<span> nos produtos</span>
+          </p>
+          <button>
+            <span>Ver produto</span>
+          </button>
+        </div>
+      </section>
+
+      <section className="categories">
+        <ul className="categories-content">
+          <li>
+            <div className="category-marked">
+              <img src={tecnologyIcon} alt="" aria-hidden="true" />
+            </div>
+            <h2>
+              <span>Tecnologia</span>
+            </h2>
+          </li>
+
+          <li>
+            <div>
+              <img src={superMarketIcon} alt="" aria-hidden="true" />
+            </div>
+            <h2>Supermercado</h2>
+          </li>
+          <li>
+            <div>
+              <img src={drinkIcon} alt="" aria-hidden="true" />
+            </div>
+            <h2>Bebidas</h2>
+          </li>
+          <li>
+            <div>
+              <img src={toolsIcon} alt="" aria-hidden="true" />
+            </div>
+            <h2>Ferramentas</h2>
+          </li>
+          <li>
+            <div>
+              <img src={healthIcon} alt="" aria-hidden="true" />
+            </div>
+            <h2>Saúde</h2>
+          </li>
+          <li>
+            <div>
+              <img src={sportIcon} alt="" aria-hidden="true" />
+            </div>
+            <h2>Esportes e Fitness</h2>
+          </li>
+          <li>
+            <div>
+              <img src={fashionIcon} alt="" aria-hidden="true" />
+            </div>
+            <h2>Moda</h2>
+          </li>
+        </ul>
+      </section>
+    </main>
+  );
+}
