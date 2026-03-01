acd547f0923662a6953a203e7511712f70607c0f
Luan <luanitachi@hotmail.com>
2026-02-28 21:01:46 -0300

    create header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,97 @@
+import "./header.scss";
+import shield from "../../assets/icons/ShieldCheck.svg";
+import truck from "../../assets/icons/Truck.svg";
+import creditCard from "../../assets/icons/CreditCard.svg";
+import econverseHeader from "../../assets/images/Econverse-header.svg";
+import magnifyingGlass from "../../assets/icons/MagnifyingGlass.svg";
+import boxIcon from "../../assets/icons/Box.svg";
+import hearthIcon from "../../assets/icons/Heart.svg";
+import userIcon from "../../assets/icons/UserCircle.svg";
+import cartIcon from "../../assets/icons/ShoppingCart.svg";
+import crowIcon from "../../assets/icons/CrownSimple.svg";
+export function Header() {
+  return (
+    <header className="header">
+      <div className="container">
+        <div className="top-bar">
+          <ul className="top-bar_list">
+            <li>
+              <img src={shield} alt="" aria-hidden="true" />
+              <p>
+                Compra <span>100% segura</span>
+              </p>
+            </li>
+            <li>
+              <img src={truck} alt="" aria-hidden="true" />
+              <p>
+                <span>Frete grátis</span> acima de R$ 200
+              </p>
+            </li>
+            <li>
+              <img src={creditCard} alt="" aria-hidden="true" />
+
+              <p>
+                <span>Parcele</span> suas compras
+              </p>
+            </li>
+          </ul>
+        </div>
+
+        <div className="main-header">
+          <div className="main-header_image">
+            <img src={econverseHeader} alt="Logo Econverse" />
+          </div>
+          <div className="main-header_input">
+            <input type="text" placeholder="O que você está buscando?" />
+            <button>
+              <img src={magnifyingGlass} alt="Ícone de lupa" />
+            </button>
+          </div>
+          <div className="main-header_icons">
+            <ul>
+              <li>
+                <button aria-label="Abrir entregas">
+                  <img src={boxIcon} alt="" />
+                </button>
+              </li>
+              <li>
+                <button aria-label="Abrir favoritos">
+                  <img src={hearthIcon} alt="" />
+                </button>
+              </li>
+              <li>
+                <button aria-label="Abrir usuário">
+                  <img src={userIcon} alt="" />
+                </button>
+              </li>
+              <li>
+                <button aria-label="Abrir carrinho">
+                  <img src={cartIcon} alt="" />
+                </button>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="bottom-bar">
+          <ul>
+            <li>
+              <p>Todas Categorias</p>
+            </li>
+            <li>Supermercado</li>
+            <li>Livros</li>
+            <li>Moda</li>
+            <li>Lançamentos</li>
+            <li>
+              <span>Ofertas do dia</span>
+            </li>
+            <li>
+              <img src={crowIcon} alt="" aria-hidden="true" />
+              Assinatura
+            </li>
+          </ul>
+        </div>
+      </div>
+    </header>
+  );
+}
